de01427d19b24b0be8963a7870c3f7824cdda151
sw33ws <gillisl22@outlook.com>
2022-09-24 14:15:43 -0400

    added skills, fixed a lot more media

--- src/components/pageParts/skills.js
+++ src/components/pageParts/skills.js
@@ -6,27 +6,38 @@ function Skills(){
         <div id='skillsbackgroundcolor' className='row'>
             <h1 className='title1'>Skills</h1>
 
+            <div className='col d-none d-md-block'></div>
+
             <div className='col'>
-            <ul><h2 className='text1'><span id="skillsHighLight">Languages</span> 
-                <li>HTML5</li>
-                <li>CSS</li>
-                <li>Javascript</li>
-                <li>Node Js</li>
-                <li>MongoDB</li>
-                <li>MySQL</li>
-                <li>GraphicQL</li>
-                <li>React</li>
-            </h2></ul>
+                <div className='listbox'>
+            <h2 className='text1'><span id="skillsHighLight">Languages</span> 
+                <div className='grid'>
+                    <li className='listItems'>HTML5</li>
+                    <li className='listItems'>CSS</li>
+                    <li className='listItems'>Javascript</li>
+                    <li className='listItems'>Node Js</li>
+                    <li className='listItems'>MongoDB</li>
+                    <li className='listItems'>MySQL</li>
+                    <li className='listItems'>GraphicQL</li>
+                    <li className='listItems'>React</li>
+                </div>
+            </h2>
+            </div>
             </div>
 
             <div className='col'>
-            <ul><h2 className='text1'><span id="skillsHighLight">Applications</span>
-                <li>BootStrap</li>
-                <li>Express</li>
-                <li>JQuery</li>
-                <li>TailWind</li>
-            </h2></ul>
+                <div className='listbox'>
+            <h2 className='text1'><span id="skillsHighLight">Applications</span>
+                    <li className='listItems'>BootStrap</li>
+                    <li className='listItems'>Express</li>
+                    <li className='listItems'>JQuery</li>
+                    <li className='listItems'>TailWind</li>
+            </h2>
             </div>
+            </div>
+
+            <div className='col d-none d-md-block'></div>
+
         </div>
     )
 }
