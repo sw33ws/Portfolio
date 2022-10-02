2a34ca4320d5044cdb2bf2bf23cdb232fb76280e
sw33ws <gillisl22@outlook.com>
2022-10-02 03:46:54 -0400

    heroku deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/portfolio',
+  // process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/portfolio',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/guarded-brushlands',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
