9e2be29eefbcb92d8e3da317075ed8132fe0b8e7
sw33ws <gillisl22@outlook.com>
2023-03-25 10:30:36 -0400

    updated portfolio

--- client/src/components/oldnav/NavTabs.js
+++ client/src/components/oldnav/NavTabs.js
@@ -2,32 +2,6 @@ import React from 'react';
 import {Link} from 'react-scroll'
 import './NavTabs.css';
 
-// var x = window.matchMedia("(max-width: 1000px)")
-
-// function AboutMe() {
-//     window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-// }
-
-// function Work() {
-//     if (x.matches) {
-//         window.scrollTo({top: 200, left: 0, behavior: 'smooth'});
-//     } else {
-//         window.scrollTo({top: 750, left: 0, behavior: 'smooth'});
-//     }
-// }
-
-// function Skills() {
-//     if (x.matches) {
-//         window.scrollTo({top: 550, left: 0, behavior: 'smooth'});
-//     } else {
-//         window.scrollTo({top: 2000, left: 0, behavior: 'smooth'});
-//     }
-// }
-
-// function Contact() {
-//     window.scrollTo({top: 2500, left: 0, behavior: 'smooth'});
-// }
-
 function hamburger() {
     document.querySelector(".hamburger").classList.toggle("active")
     document.querySelector(".hamburger-menu").classList.toggle("active")
@@ -45,11 +19,6 @@ function NavTabs(){
                 <span className='bar'></span>
                 <span className='bar'></span>
             </div>
-
-            {/* <li className="NavItem" onClick={AboutMe}>About me</li>
-            <li className="NavItem" onClick={Work}>Work</li>
-            <li className="NavItem" onClick={Skills}>Skills</li>
-            <li className="NavItem" onClick={Contact}>Contact</li> */}
 
             <div className='hamburger-menu'>
                 <li className="NavItem" ><Link to="Aboutme" spy={true} smooth={true} offset={50} duration={500}>About me</Link></li>
